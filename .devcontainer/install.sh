7bb47a86bbb362efadf38cf0ec85a19b30f7b1f0
erfhipsta <erfawnalt2@atomicmail.io>
2026-05-13 01:16:19 +0330

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,11 +1,29 @@
-#!/bin/sh
-
-echo "downloading xray"
-wget -O ${PWD}/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip
-
-echo "installing"
-unzip xray.zip && chmod +x xray
-mv xray /usr/local/bin/xray
-
-rm -rf ${PWD}/*
-echo "installed!"
+{
+  "name": "echo-gate-df6462",
+  "build": {
+    "dockerfile": "Dockerfile",
+    "context": "."
+  },
+  "features": {
+    "ghcr.io/devcontainers/features/common-utils:2": {
+      "installZsh": false
+    },
+    "ghcr.io/devcontainers/features/github-cli:1": {
+      "version": "latest"
+    }
+  },
+  "postAttachCommand": "gh codespace ports visibility 443:public -c $CODESPACE_NAME && bash /usr/local/bin/start.sh",
+  "portsAttributes": {
+    "443": {
+      "label": "echo-gate-df6462",
+      "protocol": "http",
+      "onAutoForward": "silent"
+    }
+  },
+  "forwardPorts": [
+    443
+  ],
+  "hostRequirements": {
+    "cpus": 2
+  }
+}
